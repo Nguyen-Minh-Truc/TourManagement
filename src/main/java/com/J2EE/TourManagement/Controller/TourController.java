789e290250a7c4844da2d71624597872e7375969
Retsera <3122410309.sv.sgu.edu.vn>
2025-11-10 15:54:58 +0700

    123

--- src/main/java/com/J2EE/TourManagement/Controller/TourController.java
+++ src/main/java/com/J2EE/TourManagement/Controller/TourController.java
@@ -76,7 +76,7 @@ public class TourController {
 
 
     //Create
-    @PostMapping(consumes = {"application/json", "application/json;charset=UTF-8"})
+    @PostMapping
     @ApiMessage("Thêm tour thành công.")
     public ResponseEntity<TourDTO> postNewTour(@Valid @RequestBody TourCreateDTO tourCreateDTO) {
         Tour tour = tourService.handleSave(tourCreateDTO);
